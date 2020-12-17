d3d871906f4fd08b915af93f7c7246206da38b63
Joe Mayo <jmayo@mayosoftware.com>
2020-12-16 20:09:04 -0800

    Updated Section 03-05 demo.

--- CSharp9Cookbook/Chapter03/Section-03-05/Delivery.cs
+++ CSharp9Cookbook/Chapter03/Section-03-05/Delivery.cs
@@ -5,5 +5,7 @@
         public const string NextDay = "Next Day";
         public const string Standard = "Standard";
         public const string LowFare = "Low Fare";
+
+        public const int StandardDays = 7;
     }
 }
--- CSharp9Cookbook/Chapter03/Section-03-05/Program.cs
+++ CSharp9Cookbook/Chapter03/Section-03-05/Program.cs
@@ -14,8 +14,27 @@ namespace Section_03_05
                 new Order { DeliveryInstructions = Delivery.Standard },
             };
 
-            orders.ForEach(order => 
-                Console.WriteLine(order.DeliveryInstructions));
+            foreach (var order in orders)
+            {
+                int days;
+
+                switch (order.DeliveryInstructions)
+                {
+                    case Delivery.LowFare:
+                        days = 15;
+                        break;
+                    case Delivery.NextDay:
+                        days = 1;
+                        break;
+                    case Delivery.Standard:
+                    default:
+                        days = Delivery.StandardDays;
+                        break;
+                }
+                        
+                Console.WriteLine(order.DeliveryInstructions);
+                Console.WriteLine($"Expected Delivery Day(s): {days}");
+            }
         }
     }
 }
