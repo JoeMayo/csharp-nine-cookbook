4cf2a86cb16b42b8db7cbfa3c50d27d449b04852
Joe Mayo <jmayo@mayosoftware.com>
2021-05-14 23:06:14 -0700

    Skeleton code for Chapter 9 and completed sections 1 and 2.

---
+++ CSharp9Cookbook/Chapter09/Section-09-02/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_09_02
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+}
---
+++ CSharp9Cookbook/Chapter09/Section-09-02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Section_09_02
+{
+    class Program
+    {
+        // doesn't work
+        // var address = new Address();
+
+        // this still works
+        Address addressOld = new Address();
+
+        // new target typed field
+        Address addressNew = new();
+
+        static void Main(string[] args)
+        {
+            // these still work
+            var addressLocalVar = new Address();
+            Address addressLocalOld = new Address();
+
+            // new target typed local variable
+            Address addressLocalNew = new();
+        }
+    }
+}
